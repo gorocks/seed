1ed84eec1da8dd6da00eb1f69de4ee54a3a71c0a
luanxiaowang <luanxiaowang@guazi.com>
2018-01-26 23:34:05 +0800

    init

---
+++ logger/color/colorwriter.go
@@ -0,0 +1,13 @@
+
+package colors
+
+import "io"
+
+type colorWriter struct {
+	w    io.Writer
+	mode outputMode
+}
+
+func (cw *colorWriter) Write(p []byte) (int, error) {
+	return cw.w.Write(p)
+}
